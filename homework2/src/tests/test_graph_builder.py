046e2366f75e0cf84d174240a014d519dc1fbc83
guezwhozbak <123881859+guezwhozbak@users.noreply.github.com>
2024-11-17 13:12:04 +0300

    Add files via upload

---
+++ homework2/src/tests/test_graph_builder.py
@@ -0,0 +1,18 @@
+import unittest
+from src.graph_builder import build_graph
+from git import Repo
+from datetime import datetime, timezone
+
+class TestGraphBuilder(unittest.TestCase):
+    def setUp(self):
+        self.repo_path = r'C:\Users\guezwhozbak\Desktop\graphviz\configuration-upravlation'
+        self.date = '2023-01-01'
+
+    def test_build_graph(self):
+        dot = build_graph(self.repo_path, self.date)
+        self.assertIsNotNone(dot)
+        self.assertTrue(len(dot.source) > 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
