49366d195f1e2267c034f014d00bec4124f54d02
guezwhozbak <123881859+guezwhozbak@users.noreply.github.com>
2024-11-18 01:26:24 +0300

    Add files via upload

--- homework2/git-commit-visualizer/src/tests/test_graph_builder.py
+++ homework2/git-commit-visualizer/src/tests/test_graph_builder.py
@@ -5,11 +5,13 @@ from datetime import datetime, timezone
 class TestGraphBuilder(unittest.TestCase):
     def setUp(self):
         self.repo_path = r'C:\Users\guezwhozbak\Desktop\graphviz\configuration-upravlation'
-        self.date = '2023-01-01'
+        self.date = '2024-11-17'
 
     def test_build_graph(self):
         dot = build_graph(self.repo_path, self.date)
+        # Проверка, что результат не none
         self.assertIsNotNone(dot)
+        # Проверка, что длина строки dot.source > 0
         self.assertTrue(len(dot.source) > 0)
 
 if __name__ == '__main__':
